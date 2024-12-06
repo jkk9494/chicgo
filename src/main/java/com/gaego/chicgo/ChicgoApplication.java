2c5c48b8b6e2a9d9c0b01f045426b8a3932be257
kimjunki <rlawnsrl17@gmail.com>
2024-12-06 11:33:01 +0900

    2024 12 06 스프링부트 기본 설정 추가

---
+++ src/main/java/com/gaego/chicgo/ChicgoApplication.java
@@ -0,0 +1,13 @@
+package com.gaego.chicgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaego/chicgo/ChicgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaego.chicgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
